60dfe6ca72b0c740a507cc80057214dbd89e220e
michalskimateusz <michalskimateusz@outlook.com>
2022-09-28 19:04:52 +0200

    Song card component functionality

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { FaPauseCircle, FaPlayCircle } from "react-icons/all";
 
-const PlayPause = () => {
-  return <div>PlayPause</div>;
-};
+const PlayPause = ({ isPlaying, song, activeSong, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} />
+  ) : (
+    <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />
+  );
 
 export default PlayPause;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants.js";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore.js";
+import { useDispatch, useSelector } from "react-redux";
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
 
   if (isFetching) return <Loader title="Loading Songs..." />;
   if (error) return <Error />;
-
-  console.log(data);
 
   return (
     <div className="flex flex-col">
@@ -34,7 +35,14 @@ const Discover = () => {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
         ))}
       </div>
     </div>
