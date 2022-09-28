d8f78bff14208ecff0b5a8438093fcb010cc3a7e
michalskimateusz <michalskimateusz@outlook.com>
2022-09-28 15:09:13 +0200

    Connecting to API

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import playerReducer from "./features/playerSlice.js";
+import { shazamCoreApi } from "./services/shazamCore.js";
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
