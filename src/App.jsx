ef0d37295eeab5f7a0fd47512e2c862f7f07682c
michalskimateusz <michalskimateusz@outlook.com>
2022-09-28 14:30:11 +0200

    Discover page setup

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-[#2A2B2A] to-[#618a6d]">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-[black] to-[#618a6d]">
         <Searchbar />
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
