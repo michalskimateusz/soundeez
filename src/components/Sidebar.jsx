68df6c8f3f141750af3d3ac199206b493c9ab4ff
michalskimateusz <michalskimateusz@outlook.com>
2022-09-28 19:45:53 +0200

    Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { useState } from "react";
+import { logo } from "../assets/index.js";
+import { links } from "../assets/constants.js";
+import { NavLink } from "react-router-dom";
+import { HiOutlineMenu, RiCloseLine } from "react-icons/all";
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start item-center my-8 text-sm font-medium text-gray-400 hover:text-lime-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#0c120e]">
+        <img src={logo} alt="logo" className="w-full h-28 object-contain" />
+        <NavLinks />
+      </div>
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(false)}
+          />
+        ) : (
+          <HiOutlineMenu
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(true)}
+          />
+        )}
+      </div>
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#0c120e] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <img src={logo} alt="logo" className="w-full h-28 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
